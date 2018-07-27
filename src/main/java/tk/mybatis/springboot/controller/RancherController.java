1dd1ede7aa82342752d234c7ebdf7f9bdb635980
liweiwei <2622026762@qq.com>
2018-07-27 16:53:34 +0800

    20180727

--- src/main/java/tk/mybatis/springboot/controller/RancherController.java
+++ src/main/java/tk/mybatis/springboot/controller/RancherController.java
@@ -27,12 +27,18 @@ package tk.mybatis.springboot.controller;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import tk.mybatis.springboot.model.rancher.CreateServiceBean;
 import tk.mybatis.springboot.model.rancher.LaunchConfig;
+import tk.mybatis.springboot.model.rancher.PublicEndpoints;
+import tk.mybatis.springboot.util.HttpUtil;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author liuzh
@@ -44,7 +50,7 @@ public class RancherController {
 
 
     @RequestMapping(value = "/add")
-    public JSONObject add() {
+    public boolean add() {
         //生成参数
         CreateServiceBean createServiceBean = new CreateServiceBean();
         createServiceBean.setScale(1);
@@ -80,12 +86,53 @@ public class RancherController {
 
 
         String result = HttpRequest.post("http://106.15.58.63:8080/v2-beta/projects/1a5/services")
-                .header("23B52EF320D702860BC5", "FV7FxAx5rnQ32zDCsAeCpoTaQG3YQZqWJiRhQqVS")
+                .header("Accept", "application/json").header("23B52EF320D702860BC5", "FV7FxAx5rnQ32zDCsAeCpoTaQG3YQZqWJiRhQqVS")
                 .body(JSONUtil.toJsonPrettyStr(createServiceBean))
-                .timeout(20000)
+                .timeout(1000000)
                 .execute().body();
-
-        return null;
+        System.out.println(result);
+        JSONObject json = new JSONObject(result);
+        return json != null && json.get("state") != null && json.get("state").equals("registering");
     }
 
+    @GetMapping("/list")
+    public Map list() {
+        Map<String, List> map = new HashMap<String, List>();
+        String url = "http://106.15.58.63:8080/v2-beta/projects/1a5/services?stackId=1st5";
+        String str = HttpRequest.get(url).header("Accept", "application/json").timeout(20000).execute().body();
+        com.alibaba.fastjson.JSONObject jsonObject = (com.alibaba.fastjson.JSONObject) JSON.parse(str);
+        com.alibaba.fastjson.JSONArray data = jsonObject.getJSONArray("data");
+
+        List<Map> result = new ArrayList<Map>();
+        List<CreateServiceBean> list = data.toJavaList(CreateServiceBean.class);
+        list.forEach(it -> {
+            Map<String, String> map1 = new HashMap<String, String>();
+            map1.put("id", it.getId());
+            map1.put("name", it.getName());
+            map1.put("state", it.getState());
+            map1.put("image", it.getLaunchConfig().getImageUuid());
+            String port = "";
+            List<PublicEndpoints> pList = it.getPublicEndpoints();
+            if (pList != null && pList.size() > 0) {
+                for (PublicEndpoints p : pList) {
+                    port += p.getPort() + ", ";
+                }
+                port = port.substring(0, port.length()-2);
+            }
+            map1.put("port", port);
+            result.add(map1);
+        });
+        map.put("result", result);
+        return map;
+    }
+
+    @RequestMapping(path = "/del/{id}", method = RequestMethod.DELETE)
+    public boolean del(@PathVariable String id) {
+        if (StringUtils.isNotBlank(id)) {
+            String url = "http://106.15.58.63:8080/v2-beta/projects/1a5/services/"+id;
+            net.sf.json.JSONObject json = HttpUtil.httpRequest(url, "DELETE", null);
+            return json != null && json.get("state") != null && json.get("state").equals("removing");
+        }
+        return false;
+    }
 }
